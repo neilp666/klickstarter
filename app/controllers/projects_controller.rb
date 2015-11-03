d1c389be43b672d72735b61a6d2fd9091a10357c
neil patel <neil@myindianweddingapp.com>
2015-11-03 12:08:46 +0000

    Added projects create controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
      @projects = Project.all
@@ -15,5 +17,28 @@ class ProjectsController < ApplicationController
   def edit
   end
 
+  def create
+    @project = current_user.projects.build(project_params)
+
+    respond_to do |format|
+    if @project.save
+      format.html { redirect_to @project, notice: "Project was successfully created!" }
+      format.json { render :show, status: :ok, location: @project }
+    else
+      format.html { redirect_to :edit }
+      format.json { render json: @project.errors, status: :unprocessable_entitiy }
+    end
+  end
+end
+
+  private 
+
+  def set_project
+      @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :short_description, :description, :goal, :image_url, :expiration_date)
+  end
 
 end
